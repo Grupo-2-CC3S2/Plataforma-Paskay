3b3acf0444920044e59aa7d3cdaeace791161a9d
Carlos Felix Romo <carloncho2357@hotmail.com>
2021-05-14 16:25:08 -0500

    Se ha añadido react router

--- paskay-frontend/src/App.js
+++ paskay-frontend/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main'
 
 function App() {
   return (
-    <div className="App">
+    <Main/>   
+  );
+}
+
+export default App;
+
+/*
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +27,4 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+*/
---
+++ paskay-frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  
+  render(){
+  return (
+    <div>
+        Este es el About us    
+      </div>
+  )};
+}
+
+export default Home;

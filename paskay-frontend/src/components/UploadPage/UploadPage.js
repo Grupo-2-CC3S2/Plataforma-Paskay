1128b8855c2a5249dd23c1dec45be6148ae1287d
Bryan <basmatf@uni.pe>
2021-06-30 21:51:23 -0500

    actualizacion de subir pregunta

--- paskay-frontend/src/components/UploadPage/UploadPage.js
+++ paskay-frontend/src/components/UploadPage/UploadPage.js
@@ -1,6 +1,93 @@
 
 import React, {Component} from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button} from 'react-bootstrap';
+import { Collapse} from 'react-bootstrap';
+
+/////////////////////////////////////////////////////////
+
+function Example() {
+  const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+
+  return (
+    <div className='row'>
+      {/*////////////////// botones de opciones de problemas //////////////////*/}
+      <div className='col-md-6'>
+        <Button
+          className={'btn-block'}
+          onClick={() => {setOpen(true); setOpen2(false);}}
+          aria-controls="example-collapse-text"
+        aria-expanded={open}
+        >
+          Problema como imágen
+        </Button>
+      </div>
+
+      <div className='col-md-6'>
+        <Button
+          className={'btn-block'}
+          onClick={() => {setOpen(false); setOpen2(true);}}
+          aria-controls="example-collapse-text2"
+        aria-expanded={open2}
+        >
+          Problema como Texto
+        </Button>
+      </div>
+
+      {/*////////////////// Contenido prob imagen //////////////////*/}
+     
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+          <div className='row my-3'>
+            <div className="col-md-4"><h5> Archivo de imágen</h5>
+              <input className="my-3" type='file'/>
+              <p>*Asegurese que el archivo tenga formato de imágen (png, jpg, jpeg) antes de cargarlo.
+                No se aceptan archivos con otro tipo de formato*</p>
+              <button class="btn btn-danger">Detectar Texto</button>
+            </div>
+            <div className="col-md-3 abs-center">
+              <h5> Vista previa</h5>
+              <div className="card justiy-content-around" style={{width: "12rem"}}>
+                <img className="card-img-top" src={'./../../pmuestra1.png'}></img>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <h5> Texto detectado</h5>
+              <textarea className="form-control my-2" rows={'2'}></textarea>
+              <p>*Si el texto detectado (arriba) no coincide con el de la imágen, tiene la opción a editarlo*</p>
+              <button class="btn btn-success my-2">Aceptar</button>
+            </div>
+
+            <div className="col-md-12 my-3"><h3>Filtros</h3></div>
+              <div className="col-md-8 my-3">
+              <h5>Descripción</h5>
+              <textarea className=" form-control"></textarea>
+            </div>
+            <div className="col-md-4">
+            <div className="col-md-12 my-3"><h5>Palabras clave</h5></div>
+              <button className="btn btn-primary w-100">Agregar</button>       
+            </div>
+          </div>
+        </div>
+      </Collapse>
+      <Collapse in={open2}>
+        <div className='row my-3'>
+          <div className='col-md-8'>
+            <textarea className="col-md 12 form-control my-2" rows={'4'}></textarea>
+            <input className='col-md-2 mx-2' placeholder='opcion a'></input><input className='col-md-2 mx-2' placeholder='opcion b'></input>
+            <input className='col-md-2 mx-2' placeholder='opcion c'></input><input className='col-md-2 mx-2' placeholder='opcion d'></input>
+            <input className='col-md-2 mx-2' placeholder='opcion e'></input>
+          </div>
+          <div className='col-md-4'>
+            <input className='col-md-12 my-5' type='file'/>
+          </div>
+        </div>
+      </Collapse>
+    </div>
+  );
+}
 
 //////////////////////SAMPLE//////////////////////////////
 
@@ -97,10 +184,11 @@ class UploadPage extends Component {
 
 
   estilos={
-    color: 'black',
+    color: 'black', 
+    //backgroundColor: 'rgba(100, 255, 255, .2)',
     fontStyle: 'italic'
 
-  }; 
+  } 
 
   render(){
   return (
@@ -129,26 +217,9 @@ class UploadPage extends Component {
     {/*<form>*/}
       <div className="row">
         <div className="col-md-12 my-3"><hr/><h2>SUBIR PROBLEMA</h2><hr/></div>
-        <div className="col-md-4"><h5> Archivo de imágen</h5>
-          <input className="my-3" type='file'/>
-          <p>*Asegurese que el archivo tenga formato de imágen (png, jpg, jpeg) antes de cargarlo.
-              No se aceptan archivos con otro tipo de formato*</p>
-          <button class="btn btn-danger">Detectar Texto</button>
-        </div>
-        <hr/>
-        <div className="col-md-3 abs-center">
-          <h5> Vista previa</h5>
-          <div className="card justiy-content-around" style={{width: "12rem"}}>
-            <img className="card-img-top" src={'./../../pmuestra1.png'}></img>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <h5> Texto detectado</h5>
-          <textarea className="form-control my-2" rows={'2'}></textarea>
-          <p>*Si el texto detectado (arriba) no coincide con el de la imágen, tiene la opción a editarlo*</p>
-          <button class="btn btn-success my-2">Aceptar</button>
-        </div>
-        <div className="col-md-12 my-3"><h4>Detalles </h4></div>
+      </div>
+      <div className="row">
+      <div className="col-md-12"><h4>Detalles </h4></div>
         <div className="col-md-4 my-3">
           <h5>Curso</h5>
           <select className="form-control">
@@ -208,39 +279,16 @@ class UploadPage extends Component {
             <option selected value="coconut">OTRO</option>
           </select>
         </div>
-        <div className="col-md-12 my-3"><h3>Filtros</h3></div>
-        <div className="col-md-8 my-3">
-          <h5>Descripción</h5>
-          <textarea className=" form-control"></textarea>
-        </div>
-        <div className="col-md-4">
-          <div className="col-md-12 my-3"><h5>Palabras clave</h5></div>
-          <input
-          className="form-control my-2"
-          name="firstname"
-          value={this.state.firstname}
-          onChange={this.handleChange.bind(this)} 
-          />
-        {/*<div>Last Name</div>
-        <input
-          name="lastname"
-          value={this.state.lastname}
-          onChange={this.handleChange.bind(this)} 
-        />*/}
-        <button  
-          className="btn btn-primary w-100"
-          onClick={() => this.addItem()}>Agregar
-        
-        </button>
-        <PostList
-          postList={this.state.items}
-          removeItem={this.removeItem.bind(this)} 
-        />
-        </div>
       </div>
+      {/* ****************************************React transitions ************************************* */}
+      <div>
+        
+        <Example />
+      </div>  
+      
       <row>
         <hr/>
-        <div className="col-md-12"><input className="btn w-50 btn-primary btn-lg my-3" type="Submit"></input></div>
+        <div className="col-md-12"><input value="Subir" className="btn w-50 btn-primary btn-lg my-3" type="Submit"></input></div>
       </row>
     {/*</form>*/}
     </div>
